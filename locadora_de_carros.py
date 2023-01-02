5bc3fdea322c86696098459617cea697909bfef7
Reinaldo Silva <reinaldoblack@hotmail.com>
2023-01-02 07:51:17 -0300

    dicionarios

---
+++ locadora_de_carros.py
@@ -0,0 +1,20 @@
+print('==================')
+print('Bem vindo a locadora de Veículos')
+print('O que deseja fazer?')
+entrada = int(input('Escolha uma opção'))
+entrada_conversao = list(cliente) [entrada]
+cliente = {
+    '0': 'Mostrar portifólio |',
+    '1': 'Alugar um carro |',
+    '2': 'Devolver um carro |'
+}
+
+modelos = {
+    '[0]': 'Chevrolet Tracker',
+    '[1]': 'Chevrolet Onix',
+    '[2]': 'Hyundai HB20',
+    '[3]': 'Chevrolet Tucson',
+    '[4]': 'Fiat Uno',
+    '[5]': 'Chevrolet Mobi',
+    '[6]': 'Chevrolet Pulse'
+}
